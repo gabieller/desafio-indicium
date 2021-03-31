e19a3461c21a943002a1f86389e786dce59a3efa
gabieller <gabieller@hotmail.com>
2021-03-31 17:11:29 -0300

    styling sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,40 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Link from "next/link"
 import Image from "next/image"
 
 import styles from "../styles/NavBar.module.css"
 
-export default function NavBar({ visible }) {
+export default function NavBar() {
+  const [uri, setUri] = useState("")
+
+  useEffect(() => {
+    setUri(window.location.hash)
+  })
+
+  // const uri = window.location.hash
+
   return (
     <div className={styles.sidebar}>
       <nav className={styles.nav}>
-        <ul className={styles.text}>
+        <ul className={styles.list}>
           <Link href="#home">
-            <li>
-              <Image src="/images/home.svg" height={24} width={24} />
+            <li className={`nav-item ${uri === "#home" ? "active" : ""}`}>
+              <Image src="/images/home.svg" height={36} width={36} />
             </li>
           </Link>
           <Link href="#about">
-            <li>
-              <Image src="/images/user.svg" height={24} width={24} />
+            <li className={`nav-item ${uri === "#about" ? "active" : ""}`}>
+              <Image src="/images/user.svg" height={36} width={36} />
             </li>
           </Link>
           <Link href="#interest">
-            <li>
-              <Image src="/images/sun.svg" height={24} width={24} />
+            <li className={`nav-item ${uri === "#interest" ? "active" : ""}`}>
+              <Image src="/images/sun.svg" height={36} width={36} />
             </li>
           </Link>
           <Link href="#skills">
-            <li>
-              <Image src="/images/computer.svg" height={24} width={24} />
+            <li className={`nav-item ${uri === "#skills" ? "active" : ""}`}>
+              <Image src="/images/computer.svg" height={36} width={36} />
             </li>
           </Link>
         </ul>
