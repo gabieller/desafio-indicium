34c4f149e693f8a21108dadbaafa3d14c63a3556
gabieller <gabieller@hotmail.com>
2021-04-01 13:06:30 -0300

    Adding google analytics tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,11 +13,26 @@ import styles from "../styles/Layout.module.css"
 export default function Layout(props) {
   return (
     <>
-          <Head>
-          <title>Gabriela Silva Eller</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header />
+      <Head>
+        <title>Gabriela Silva Eller</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WB61H6RT3L"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WB61H6RT3L');
+  `,
+          }}
+        />
+      </Head>
+      <Header />
       <NavBar />
       <div className={styles.main}>{props.children}</div>
       <Footer />
